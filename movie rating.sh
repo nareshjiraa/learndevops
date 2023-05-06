ad7b73f2f5d3d428a15c03fd32b0d0af7ed311dc
NARESH <nareshjiraa@gmail.com>
2023-05-06 14:48:30 +0530

    shipping

--- movie rating.sh
+++ movie rating.sh
@@ -7,7 +7,7 @@ fi
 
 percent= $(curl -s https://www.themoviedb.org/movie/$input | grep -w user_score_chart | grep data-percent | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
-echo percent - $percent
+echo "percent - $percent"
 
 if [ "$percent" -ge 70 ]; then
   echo good
