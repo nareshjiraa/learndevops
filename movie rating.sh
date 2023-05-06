602d423f94d1e8ecfa186571ec9da9b34cdeee02
NARESH <nareshjiraa@gmail.com>
2023-05-06 13:57:48 +0530

    shipping

---
+++ movie rating.sh
@@ -0,0 +1,14 @@
+
+input=$1
+if [ -z "$input" ]; then
+  echo input name is missing
+  exit
+fi
+
+percent=$(curl -s https://www.themoviedb.org/movie/$input | grep -w user_score_chart | grep data-percent | xargs -n1 | grep data-percent | awk -F = '{print $2}')
+
+if [ "$percent" -ge 70 ]; then
+  echo good
+  else
+    echo average
+fi
