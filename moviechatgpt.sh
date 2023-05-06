fbf8b866033bdfa493e9b70848095bb574343443
NARESH <nareshjiraa@gmail.com>
2023-05-06 14:52:26 +0530

    shipping

--- moviechatgpt.sh
+++ moviechatgpt.sh
@@ -1,11 +1,26 @@
+#!/bin/bash
+
 input=$1
+
 if [ -z "$input" ]; then
   echo "Input name is missing"
   exit 1
 fi
 
-percent=$(curl -s "https://www.themoviedb.org/movie/$input" | grep -w user_score_chart | grep data-percent | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
-echo "percent - $percent"
+url="https://www.themoviedb.org/movie/$input"
+echo "URL: $url"
+
+html=$(curl -s "$url")
+echo "HTML: $html"
+
+percent=$(echo "$html" | grep -w user_score_chart | grep data-percent | xargs -n1 | grep data-percent | awk -F= '{print $2}' | awk -F. '{print $1}')
+echo "Percent: $percent"
+
+if ! [[ "$percent" =~ ^[0-9]+$ ]]; then
+  echo "Error: percent is not a number"
+  exit 1
+fi
+
 if [ "$percent" -ge 70 ]; then
   echo "Good"
 else
