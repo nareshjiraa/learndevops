badd7b053845f7b906cdf0c42b33c4256beb32c5
NARESH <nareshjiraa@gmail.com>
2023-05-06 11:30:21 +0530

    shipping

--- conditions.sh
+++ conditions.sh
@@ -2,19 +2,17 @@
 fruit_name=$1
 quantity=$2
 
-if [ fruit_name=="mango" ]; then
+if [ "$fruit_name" == "mango" ]; then
   echo mango quantity=$quantity
 
 else
   echo fruit doesnot exist
 fi
 
-if [ quantity gt 100]; then
+if [ "$quantity" -gt 100]; then
   echo mango price=1$
-
- else
-   echo mango price=2$
-
+else
+  echo mango price=2$
 fi
 
 
